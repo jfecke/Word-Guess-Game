4e212fb632a75fe61817324154cd114b9d7df96c
jfecke <jfecke@gmail.com>
2018-10-13 20:06:50 -0500

    initial commit

---
+++ assets/javascript/game.js
@@ -0,0 +1,20 @@
+document.onkeyup= function(event) {hangman.keyPress(event)};
+
+var hangman = {
+    keyPress: function(event) {
+    var key = event.key;
+    switch(key) {
+      case 'd':
+
+      case 'w':
+
+      case 'g':
+
+        break;
+      case 'h':
+
+        break;
+    }
+  }
+} 
+
